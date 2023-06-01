56aa8dd39315bbbeb18746d9ba39b7e1d7e6dfb8
ShuheiOhno <inu.suika8@gmail.com>
2023-06-02 02:38:19 +0900

    １

---
+++ practice/basic/1-5.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Waring!!\a\n");
+}
---
+++ practice/basic/1-7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    //うまくいかない
+    int x = 3.14;
+    int y = 5.9;
+
+
+    printf("x:%d\n",x);
+    printf("y:%d\n",y);
+}
