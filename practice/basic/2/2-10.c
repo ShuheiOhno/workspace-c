d67151409e8cd7baee2feeff94917a382476971a
ShuheiOhno <inu.suika8@gmail.com>
2023-06-02 22:12:50 +0900

    q

---
+++ practice/basic/2/2-10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int a,b;
+
+    puts("二つの整数を入力せよ");
+    printf("a:"); scanf("%d", &a);
+    printf("b:"); scanf("%d", &b);
+
+    printf("aはbの%f%%\n", (double)a / b * 100);
+}
---
+++ practice/basic/2/2-11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    // 上底、下底、高さ
+    int a, b, height;
+
+    printf("台形の面積\n");
+    printf("上底"); scanf("%d", &a);
+    printf("下底"); scanf("%d", &b);
+    printf("高さ"); scanf("%d", &height);
+
+    printf("面積:%lf", (double)(a + b) * height /2);
+
+}
