1063a2e3cb30f6c71de548f712a8daa80e955f50
ShuheiOhno <inu.suika8@gmail.com>
2023-06-04 07:26:02 +0900

    1

---
+++ practice/basic/11/11-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    char a[][5] = {"LISP", "C", "Ada"};
+    char *p[] = {"PAUL", "X", "MAC"};
+    for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++){
+        printf("a[%d] = \"%s\"\n", i, a[i]);
+    }
+    for (int i = 0; i < sizeof(p)/ sizeof(p[0]); i++){
+        printf("p[%d] = \"%s\"\n", i, p[i]);
+    }
+
+    //charの検証
+    char b[5] = {"afff"};
+    for (int i = 0; i < sizeof(b)/ sizeof(b[0]); i++){
+        printf("b[%d] = %c\n", i, b[i]);
+    }
+    return 0;
+}
