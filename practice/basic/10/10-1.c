90e2c941b8fdfc97e963b6e4a24fd909b0fef89a
ShuheiOhno <inu.suika8@gmail.com>
2023-06-04 01:36:40 +0900

    1

---
+++ practice/basic/10/10-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void adjust_point(int *n){
+    if (*n < 0) *n = 0;
+    if (*n > 100) *n = 100;
+}
+void adjust_point2(int n){
+    if (n < 0) n = 0;
+    if (n > 100) n = 100;
+}
+
+int main(void){
+    int point;
+    printf("テストの点数");
+    scanf("%d", &point);
+
+    adjust_point2(point);
+    printf("点数は%d点です\n", point);
+    adjust_point(&point);
+    printf("点数は%d点です\n", point);
+    return 0;
+
+}
---
+++ practice/basic/10/10-2.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+//閏年の判定
+int is_leap(int y){
+    return y % 4 == 0 && y % 100 != 0 || y % 400 == 0; 
+}
+
+//y年m月の日数
+int days_of_month(int y, int m){
+    int mdays[][12] = {
+        {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+        {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},//閏年
+    };
+    return mdays[is_leap(y)][m - 1];
+}
+
+//y年m月d日を前日以前の日付に更新
+void decrement_date(int *y, int *m, int *d){
+    if(*d > 1){
+        --*d;
+    } else {
+        if(--*m < 1){
+            --*y;
+            *m = 12;
+        }
+        *d = days_of_month(*y, *m);
+    }
+}
+
+//y年m月d日を翌日以降の日付に更新
+void increment_date(int *y, int *m, int *d){
+    if(*d < days_of_month(*y, *m)){
+        ++*d;
+    } else {
+        if (++*m > 12){
+            ++*y;
+            *m = 1;
+        }
+        *d = 1;
+    }
+}
+
+int main(void){
+    int n;
+    int y, m, d;
+
+    puts("日付を入力");
+    printf("年:"); scanf("%d", &y);
+    printf("月:"); scanf("%d", &m);
+    printf("日:"); scanf("%d", &d);
+
+    printf("何日戻しますか:"); scanf("%d", &n);
+    for (int i = 0; i < n; i++){
+        decrement_date(&y, &m, &d);
+    }
+    printf("%d年%d月%d日になりました\n", y, m, d);
+
+    printf("何日進めますか:"); scanf("%d", &n);
+    for (int i = 0; i < n; i++){
+        increment_date(&y, &m, &d);
+    }
+    printf("%d年%d月%d日になりました\n", y, m, d);
+
+
+    return 0;
+}
