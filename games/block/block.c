2d98dde9df37a1d104f9fe43568b017325218e18
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-11-11 22:08:57 +0900

    f

--- games/block/block.c
+++ games/block/block.c
@@ -1,5 +1,26 @@
+#include <stdio.h>
 #include <conio.h>
 
+#define FIELD_WIDTH (14)
+#define FIELD_HEIGHT (FIELD_WIDTH * 2)
+
 int main() {
+    for (int i = 0; i < FIELD_WIDTH + 2; i++)
+        printf("a");
+    printf("\n");
+
+    for (int i = 0; i < FIELD_HEIGHT; i++){
+        printf("a");
+        for(int j = 0; j < FIELD_WIDTH; j++)
+            printf(" ");
+        printf("a");
+        printf("\n");
+    }
+
+    for(int i = 0; i < FIELD_WIDTH + 2; i++)
+        printf("a");
+    
+
+        
     _getch();
 }
