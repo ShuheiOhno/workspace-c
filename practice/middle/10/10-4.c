f2d5d76411837b6fb56f9847a7ef570f2f44dbe3
ShuheiOhno <inu.suika8@gmail.com>
2024-01-04 19:14:27 +0900

    Update 10-4.c

--- practice/middle/10/10-4.c
+++ practice/middle/10/10-4.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(void){
+    int num;
+    printf("文字列は何個: ");
+    scanf("%d", &num);
+    char (*p)[15] = (char (*)[15])malloc(num * 15);
+    if (p == NULL) {
+        puts("記憶域の確保に失敗しました\n");
+    } else {
+        for (int i = 0; i < num; i++) { //文字列を読み込む
+            printf("p[%d] : ", i);
+            scanf("%s", p[i]);
+        }
+        for (int i = 0; i < num; i++) { //文字列を表示
+            printf("p[%d] = %s\n", i, p[i]);
+        }
+        free(p);
+    }
+    return 0;
+}
