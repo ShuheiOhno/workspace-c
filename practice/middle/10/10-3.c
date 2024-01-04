10e37c3f0866c2d1f15e611ec9cc058665b3d88b
ShuheiOhno <inu.suika8@gmail.com>
2024-01-04 19:08:57 +0900

    k

---
+++ practice/middle/10/10-3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void){
+    char str[16];
+
+    printf("文字列strを入力せよ: ");
+    scanf("%s", str);
+
+    char*ptr = malloc(strlen(str) + 1); //記憶域を動的に確保
+
+    if (ptr) {
+        strcpy(ptr, str); //文字列をコピー
+        printf("文字列strの複製ptrを作りました\n");
+        printf("str = %s\n", str);
+        printf("ptr = %s\n", ptr);
+        free(ptr); //記憶域を解放
+    }
+    
+    return 0;
+}
