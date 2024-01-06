46a03bbf2e58389b7356285b53607a1cb5f8d264
ShuheiOhno <inu.suika8@gmail.com>
2024-01-06 20:54:42 +0900

    Create 7-1.c

---
+++ practice/middle/7/7-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    int dgt[9] = {1,2,3,4,5,6,7,8,9};
+    int a[9] = {0};
+
+    for (int i = 0; i < 9; i++)
+        a[i] = dgt[i];
+
+    for (int i = 0; i < 9; i++)
+        printf("%d", a[i]);
+
+    putchar('\n');
+
+    return 0;
+}
