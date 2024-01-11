b6a5e0e054edc2f2a24b38912335ad9bc86997b0
ShuheiOhno <inu.suika8@gmail.com>
2024-01-11 19:14:38 +0900

    f

--- practice/middle/7/7-10.c
+++ practice/middle/7/7-10.c
@@ -6,7 +6,7 @@ int aleart_printf(const char *format, ...){
     va_list ap;
     putchar('\a');
     va_start(ap, format);
-    int count = vprintf(format, ap); //可変個引数の処理をvprintfに一任 vprintfの引数は可変個引数ではない
+    int count = vprintf(format, ap); //可変個引数の処理をvprintfに一任 vprintfの引数は可変個引数を取るが、この関数の引数自体が可変個引数ではない
     va_end(ap);
     return count;
 }
