60d05247116af033f911bb5e19d070686a67c469
ShuheiOhno <inu.suika8@gmail.com>
2024-01-12 21:21:13 +0900

    Create 7-11.c

---
+++ practice/middle/7/7-11.c
@@ -0,0 +1,10 @@
+//リアルタイムキー入力
+#include <ctype.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "getputch.h"
+
+#define MAX_STAGE 10
+#define swap(type, x, y) do { type t = x; x = y; y = t;} while(0)
+
