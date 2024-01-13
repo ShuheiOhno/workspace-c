ffe7874bd4b37ac8cdf8951b31e656f0d2c279ea
ShuheiOhno <inu.suika8@gmail.com>
2024-01-13 20:17:42 +0900

    Create mem_arr.c

---
+++ practice/middle/7/mem_arr.c
@@ -0,0 +1,40 @@
+// memcpyとmemmoveの例
+#include <stdio.h>
+#include <string.h>
+
+// 配列bのn個の要素をaにコピー
+int arycpy(int a[], const int b[], int n) {
+    if (n <= 0)
+        return 1;
+    else {
+        memcpy(a ,b ,n * sizeof(int));
+        return 0;
+    }
+}
+
+// a[idx]にxを挿入して以降の要素を一つずつずらす
+int aryins(int a[], int n, int idx, int x) {
+    if(idx < 0 || idx >= n)
+        return 1;
+    else {
+        memmove(&a[idx + 1], &a[idx], (n - idx - 1) * sizeof(int));
+        a[idx] = x;
+        return 0;
+    }
+}
+
+#define MAX 5 //配列の要素数
+
+int main(void) {
+    int x[MAX] = {1,2,3,4,5};
+    int y[MAX] = {9,9,9,9,9};
+
+    arycpy(y, x, MAX); //xの全要素をyにコピー
+    aryins(x, MAX, 2, 10);
+    puts(" x y ");
+    puts("---------");
+    for (int i = 0; i < MAX; i++) {
+        printf("[%d] :%4d%4d\n", i, x[i], y[i]);
+    }
+    return 0;
+}
