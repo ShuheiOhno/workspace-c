a2e8d1c74823ebec87bc2b95d7f1e3cbaf32db16
ShuheiOhno <inu.suika8@gmail.com>
2024-01-14 17:14:52 +0900

    Create 7-12.c

---
+++ practice/middle/7/7-12.c
@@ -0,0 +1,12 @@
+//数字、英字の3文字の並びを完成させる
+
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "getputch.h"
+
+#define MAX_STAGE 5
+#define swap(type, x, y) do { type t = x, x = y; y = t };
+
