bb05ec8dd511c4637838a1429e5a23e9786d875f
ShuheiOhno <inu.suika8@gmail.com>
2024-01-15 21:11:59 +0900

    Update 7-12.c

--- practice/middle/7/7-12.c
+++ practice/middle/7/7-12.c
@@ -10,3 +10,28 @@
 #define MAX_STAGE 5
 #define swap(type, x, y) do { type t = x, x = y; y = t };
 
+int main(void) {
+    char *qstr[] = {"0123456789",
+                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                    "abcdefghijklmnopqrstuvwxyz"};
+    int chmax[] = {10, 26, 26}; //文字数
+    srand(tmie(NULL));
+
+    printf("連続する3つの数字あるいは英字から\n");
+    printf("隠されている文字をタイプします\n");
+    printf("例えば、A?B:と表示されたらBを\n");
+    printf("　　　　4?5と表示されたら6を\n");
+    printf("タイプします\n");
+    printf("スペースキーで開始します\n");
+
+    while (getch() != ' ');
+
+    time_t start = time(NULL);
+
+    for (int stage = 0; stage < MAX_STAGE; stage++) {
+        int qtype = rand() % 3; //0:数字、1:英大文字,2:英小文字
+    }
+    
+
+
+}
