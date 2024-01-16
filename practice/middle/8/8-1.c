29d4b050125bc1888bbdeb63a43625bd4bf893b4
ShuheiOhno <inu.suika8@gmail.com>
2024-01-16 20:40:01 +0900

    Update 8-1.c

--- practice/middle/8/8-1.c
+++ practice/middle/8/8-1.c
@@ -5,6 +5,31 @@
 #include "getputch.h"
 
 int main(void){
+    char *str = "How do you do?";
+    int len = strlen(str);
+    init_getputch();
+
+    printf("このとおりタイプする\n");
+    printf("%s\n", str);
+    fflush(stdout);
+
+    time_t start = time(NULL);
+
+    for (int i = 0; i < len; i++) {
+        int ch;
+        do {
+            ch = getch(); //キー読み込み
+            if (isprint(ch)) {
+                putch(ch);
+                if (ch != str[i])
+                    putch('\b'); //違うキーが押されたら、カーソルを一つ戻す
+            }
+        } while (ch != str[i]);
+    }
+
+    time_t end = time(NULL);
+    printf("\n%.1f秒でした\n", difftime(end, start));
+    term_getputch();
 
     return 0;
 }
