b31047d2865ae022924bff062eee020451258999
ShuheiOhno <inu.suika8@gmail.com>
2024-01-17 23:34:48 +0900

    Update 1-8.c

--- practice/pointer3/1/1-8.c
+++ practice/pointer3/1/1-8.c
@@ -1,1 +1,13 @@
 //ポインタの初期化
+#include <stdio.h>
+
+int main(void) {
+    int n = 123;
+    int *p = &n;
+
+    printf("nの値=%d\n", n);
+    printf("pの値=%d\n", p);
+    printf("*pの値=%d\n", *p);
+
+    return 0;
+}
