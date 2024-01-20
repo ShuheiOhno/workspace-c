0f8ee979dc74f761c401f54d7bdc5f13a724d11a
ShuheiOhno <inu.suika8@gmail.com>
2024-01-20 22:58:01 +0900

    Create p1-5.c

---
+++ practice/pointer3/1/p1-5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void sum_diff(int x, int y, int *wa, int *sa) {
+    wa = x + y;
+    if (x <= y)
+        sa = y - x;
+    else
+        sa = x - y;
+}
+
+int main(void) {
+    int a, b;
+    int *sum;
+    int *diff;
+    printf("整数aを入力:"); scanf("%d", &a);
+    printf("整数bを入力:"); scanf("%d", &b);
+    sum_diff(sum, diff);
+    printf("aとbの和%d", *sum);
+    printf("aとbの差%d", *diff);
+    return 0;
+}
