b9fa2d8baaf819d56e0aed54388eb534eed36a58
ShuheiOhno <inu.suika8@gmail.com>
2024-01-20 22:35:38 +0900

    Create 1-10.c

---
+++ practice/pointer3/1/1-10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap(int x, int y) {
+    int tmp = x;
+    x = y;
+    y = tmp;
+}
+
+int main(void) {
+    int a, b;
+
+    puts("二つの整数を入力してください");
+    printf("整数A:"); scanf("%d", &a);
+    printf("整数B:"); scanf("%d", &b);
+    swap(a, b);
+
+    puts("整数AとBの値を交換しました\n");
+    printf("Aの値は%dです\n", a);
+    printf("Bの値は%dです\n", b);
+
+    return 0;
+}
