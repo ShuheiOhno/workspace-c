f6660c4070b08f02d928e596f0fcaa46f8d67c48
ShuheiOhno <inu.suika8@gmail.com>
2024-01-20 22:36:18 +0900

    f

--- practice/pointer3/1/1-10.c
+++ practice/pointer3/1/1-10.c
@@ -12,7 +12,7 @@ int main(void) {
     puts("二つの整数を入力してください");
     printf("整数A:"); scanf("%d", &a);
     printf("整数B:"); scanf("%d", &b);
-    swap(a, b);
+    swap(a, b); //変わりそうで変わらない
 
     puts("整数AとBの値を交換しました\n");
     printf("Aの値は%dです\n", a);
