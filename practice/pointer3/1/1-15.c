12015c0298cfcba28e0c1267acd0ed1aa0019292
ShuheiOhno <inu.suika8@gmail.com>
2024-01-20 23:28:09 +0900

    Create 1-15.c

---
+++ practice/pointer3/1/1-15.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void scan2double(double *x, double *y) {
+    printf("1番目:"); scanf("%lf", x);
+    do {
+        printf("2番目:"); scanf("%lf", y);
+    } while(*x > *y)
+}
+
+int main(void) {
+    double a, b, c, d;
+    puts("aとbを昇順に入力せよ");
+    scan2double(&a, &b);
+    puts("cとdを昇順に入力せよ");
+    scan2double(&c, &d);
+
+    printf("aとbの差: %f\n",b-a);
+    printf("cとdの差: %f\n",d-c);
+
+
+    return 0;
+}
