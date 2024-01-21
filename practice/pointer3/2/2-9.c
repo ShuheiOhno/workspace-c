14a887d051dc093c773f8dc3a52c026f3484954a
ShuheiOhno <inu.suika8@gmail.com>
2024-01-21 17:51:44 +0900

    s

--- practice/pointer3/2/2-9.c
+++ practice/pointer3/2/2-9.c
@@ -2,12 +2,16 @@
 
 //受け取ったポインタの大きさを表示
 void func(int p[]) {
+// void func(int *p) {
     printf("sizeof(p)=%u\n", (unsigned)sizeof(p)); //ポインタ
 }
 
 int main(void) {
-    int a[5];
+    int a[10] = {0};
     printf("sizeof(a)=%u\n", (unsigned)sizeof(a)); //配列
+    printf("sizeof(a[1])=%u\n", (unsigned)sizeof(a[1])); //配列
+    printf("sizeof(int *)=%u\n", (unsigned)sizeof(int *)); //配列
+    puts("func");
     func(a);
     return 0;
 }
