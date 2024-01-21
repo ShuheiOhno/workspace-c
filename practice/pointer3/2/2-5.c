b0b315d4adb926bb7d634cc608369502bea88347
ShuheiOhno <inu.suika8@gmail.com>
2024-01-21 17:15:59 +0900

    Create 2-5.c

---
+++ practice/pointer3/2/2-5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    int a[4];
+
+    a[4] = a[1] = *(a + 2) = *(3 + a) = 7;
+    for (i = 0; i < 4; i++)
+        printf("a[%d]=%d\n", i, a[i]);
+    return 0;
+}
