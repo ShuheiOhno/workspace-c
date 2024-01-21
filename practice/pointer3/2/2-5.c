8974f913c61f612c2a6cf2b65e55b6c48ed501d1
ShuheiOhno <inu.suika8@gmail.com>
2024-01-21 17:18:59 +0900

    Update 2-5.c

--- practice/pointer3/2/2-5.c
+++ practice/pointer3/2/2-5.c
@@ -4,7 +4,7 @@ int main(void) {
     int i;
     int a[4];
 
-    0[a] = a[1] = *(a + 2) = *(3 + a) = 7;
+    0[a] = a[1] = *(a + 2) = *(3 + a) = 7; //あまり使用しない方が良い（0[i]）
     for (i = 0; i < 4; i++)
         printf("a[%d]=%d\n", i, a[i]);
     return 0;
