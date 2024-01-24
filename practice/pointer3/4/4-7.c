a3d030d544dfff269d39e0c37dd34eee6e0d4760
ShuheiOhno <inu.suika8@gmail.com>
2024-01-24 21:30:35 +0900

    Update 4-7.c

--- practice/pointer3/4/4-7.c
+++ practice/pointer3/4/4-7.c
@@ -3,6 +3,7 @@
 int main(void) {
     char str[15];
     printf("文字列を入力してください："); scanf("%s", str);
+    // printf("文字列を入力してください："); scanf("%s", &str);
     printf("あなたは\"%s\"と入力しました\n", str);
     return 0;
 }
