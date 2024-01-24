e205ae1a2583bff26f40acc2eda82a3db779e3b1
ShuheiOhno <inu.suika8@gmail.com>
2024-01-24 21:11:28 +0900

    Update 4-1.c

--- practice/pointer3/4/4-1.c
+++ practice/pointer3/4/4-1.c
@@ -2,7 +2,7 @@
 
 int main(void) {
     printf("sizeof(char)=%u\n", (unsigned)sizeof(char));
-    printf("sizeof('A')=%u\n", (unsigned)sizeof('A'));
+    printf("sizeof('A')=%u\n", (unsigned)sizeof('A')); //文字定数は、int型と同じ
     printf("sizeof(int)=%u\n", (unsigned)sizeof(int));
     return 0;
 }
