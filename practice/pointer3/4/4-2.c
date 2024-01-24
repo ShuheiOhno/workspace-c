681739ce60f39356dd986bed5a4f953d7174d12e
ShuheiOhno <inu.suika8@gmail.com>
2024-01-24 21:14:57 +0900

    t

--- practice/pointer3/4/4-2.c
+++ practice/pointer3/4/4-2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("sizeof(\"\") = %u\n", (unsigned)sizeof(" "));
+    printf("sizeof(\"\") = %u\n", (unsigned)sizeof(""));
     printf("sizeof(\"abc\") = %u\n", (unsigned)sizeof("abc"));
     printf("sizeof(\"UVW\\0XYZ\") = %u\n", (unsigned)sizeof("UVW\0XYZ"));
 
