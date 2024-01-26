4bc901e1e8998f79223edb93c79d71878c4267c7
ShuheiOhno <inu.suika8@gmail.com>
2024-01-26 23:16:41 +0900

    l

--- practice/pointer3/4/p4-6.c
+++ practice/pointer3/4/p4-6.c
@@ -1,23 +1,30 @@
-#include <stdio.h>
+    #include <stdio.h>
 
-int str_dignum(const char *s) {
-    char *p = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-    int count = 0;
-    while(*s) {
-        while (*p) {
-            if(s[0] == p[0])
-                count++;
-            *p++;
+    int str_dignum(const char *s) {
+        // char *p = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        char * p = "0123456789";
+        int count = 0;
+        while(*s) {
+            p = "0123456789"; //初期化
+            while (*p) {
+                // printf("%c=%c\n",s[0], p[0]);
+                if(s[0] == p[0]) {
+                    // printf("%c", p[0]);
+                    count++;
+                }
+                p++;
+            }
+            // putchar('u');
+            s++;
+            // printf("*s:%c\n", s[0]);
         }
-        *s++;
+        return count;
     }
-    return count;
-}
 
-int main(void) {
-    char str[128];
-    printf("文字列入力:"); scanf("%s", str);
-    printf("数字文字の数:%d\n", str_dignum(str));
+    int main(void) {
+        char str[128];
+        printf("文字列入力:"); scanf("%s", str);
+        printf("数字文字の数:%d\n", str_dignum(str));
 
-    return 0;
-}
+        return 0;
+    }
