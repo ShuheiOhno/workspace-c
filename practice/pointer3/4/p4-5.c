bd401b29bbe4af626bc6f6694ba109db2274302c
ShuheiOhno <inu.suika8@gmail.com>
2024-01-26 22:51:04 +0900

    d

--- practice/pointer3/4/p4-5.c
+++ practice/pointer3/4/p4-5.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
-int str_chnum(const char *s, char c) {
+int str_chnum(const char *s, int c) {
     int count = 0;
-    while(*s) {
-        *s++;
-        if(s[0] == c)
+    while(*s != '\0') {
+        printf("%c:", s[0]);
+        if(s[0] == c) {
+            printf("OK\n");
             count++;
+        }
+        *s++;
     }
     return count;
 }
 
 int main(void) {
     char str[128];
-    char search_str;
+    // char search_str;
+    // int search_str; だめ
     printf("文字を入力:"); scanf("%s", str);
-    printf("検索する文字:"); scanf("%s", search_str);
-    printf("%sの中に%sは%d個含まれる\n",str ,search_str ,str_chnum(str, search_str));
+    // printf("検索する文字:"); scanf("%d", search_str);
+    // printf("%sの中に%cは%d個含まれる\n",str ,search_str ,str_chnum(str, search_str));
+    printf("\n%sの中に%cは%d個含まれる\n",str ,'a' ,str_chnum(str, 'a'));
     return 0;
 }
