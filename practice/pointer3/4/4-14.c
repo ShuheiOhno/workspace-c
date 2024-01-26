6ab9a6490795943b5b3253e3e56f3b15c113b364
ShuheiOhno <inu.suika8@gmail.com>
2024-01-26 22:21:47 +0900

    Create 4-14.c

---
+++ practice/pointer3/4/4-14.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int str_len(const char s[]) {
+    int len = 0;
+    while(s[len] != 0)
+        len++;
+    return len;
+}
+
+int main(void) {
+    char str[128];
+    printf("文字列を入力してください:");
+    scanf("%s", str);
+    printf("文字列%sの長さは、%d\n", str, str_len(str));
+    return 0;
+}
