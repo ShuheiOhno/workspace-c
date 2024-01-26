6431521478ae998dd2f5280f0bab7168f6a0b182
ShuheiOhno <inu.suika8@gmail.com>
2024-01-26 22:19:04 +0900

    Update 4-13.c

--- practice/pointer3/4/4-13.c
+++ practice/pointer3/4/4-13.c
@@ -4,7 +4,7 @@ int main(void) {
     char s[10];
     printf("文字列を入力してください:");
     scanf("%s", s);
-    if(s == "ABC")
+    if(s == "ABC") //sの先頭文字と"ABC"の先頭文字のアドレスが等しいか。判定は成立しない
         puts("ABCと入力");
     else
         puts("ABCではない");
