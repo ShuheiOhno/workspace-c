339a7cfdfb37ec32a65f64ad1694a687844f7784
ShuheiOhno <inu.suika8@gmail.com>
2024-01-27 11:38:40 +0900

    Create 4-18.c

---
+++ practice/pointer3/4/4-18.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void str_copy(char d[], const char s[]) {
+    int i = 0;
+    while((d[i] == s[i]) != '\0')
+        i++;
+}
+
+int main(void) {
+    char str[128] = "ABC";
+    char tmp[128];
+
+    printf("文字列strは%sです\n", str);
+    printf("コピーする文字列は：");
+    scanf("%s", tmp);
+
+    str_copy(str, tmp);
+    printf("str:%s\n", str);
+
+    return 0;
+}
+
