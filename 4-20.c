252a09ed73dbcd2c71ee81ac88fc9d837ca3d747
ShuheiOhno <inu.suika8@gmail.com>
2024-01-27 11:52:25 +0900

    Create 4-20.c

---
+++ 4-20.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap(char **x, char **y) {
+    char **temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main(void) {
+    char s1 = "ABC";
+    char s2 = "DEF";
+
+    printf("文字列s1は%sです\n", s1);
+    printf("文字列s2は%sです\n", s2);
+
+    swap(&s1, &s2);
+
+    puts("ポインタs1とs2を交換");
+
+    printf("文字列s1:%s\n", s1);
+    printf("文字列s2:%s\n", s2);
+}
