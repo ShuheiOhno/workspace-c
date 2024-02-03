8bcc06b24b27cac9184ad43c180b14d2ba28acd7
ShuheiOhno <inu.suika8@gmail.com>
2024-02-03 22:05:39 +0900

    Update 4-51.c

--- practice/pointer3/4/4-51.c
+++ practice/pointer3/4/4-51.c
@@ -1,6 +1,6 @@
 // mencpy
 #include <stdio.h>
-void *memcpy(void *s1, const void *s2, size_t) {
+void *memcpy(void *s1, const void *s2, size_t n) {
     char *p1 = (char *)s1;
     const char *p2 = (const char *)s2;
     while (n-- > 0)
