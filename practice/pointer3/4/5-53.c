2c92018f1a2acf94834f72e04bbf04ad744088a2
ShuheiOhno <inu.suika8@gmail.com>
2024-02-03 22:21:15 +0900

    Create 5-53.c

---
+++ practice/pointer3/4/5-53.c
@@ -0,0 +1,36 @@
+// memcpy memmove
+#include <stdio.h>
+#include <string.h>
+
+int arycpy(int a[], const int b[], int n) {
+    if (n <= 0)
+        return 1;
+    else {
+        memcpy(a, b, n * sizeof(int));
+    }
+}
+
+// a[idx]にxを挿入して以降の要素を一つずつずらす
+int aryins(int a[], int n, int idx, int x) {
+    if (idx < 0 || idx >= n)
+        return 1;
+    else {
+        memmove(&a[idx + 1], &a[idx], (n - idx -1) * sizeof(int));
+        a[idx] = x;
+        return 0;
+    }
+}
+
+#define MAX 5
+
+int main(void) {
+    int x[MAX] = {1,2,3,4,5};
+    int y[MAX] = {5,5,5,5,5};
+    arycpy(y, x, MAX);
+    aryins(x, MAX, 2, 10);
+    puts("           x  y ");
+    puts("----------------");
+    for (int i = 0; i < MAX; i++)
+        printf("[%d] :%4d%4d\n", i, x[i], y[i]);
+    return 0;
+}
