55add95fe0bcf89795807df13676add8874fe7a3
ShuheiOhno <inu.suika8@gmail.com>
2024-02-03 21:57:08 +0900

    Create 4-51.c

---
+++ practice/pointer3/4/4-51.c
@@ -0,0 +1,12 @@
+// mencpy
+#include <stdio.h>
+void *memcpy(void *s1, const void *s2, size_t) {
+    char *p1 = (char *)s1;
+    const char *p2 = (const char *)s2;
+    while (n-- > 0)
+    {
+        *p1++;
+    }
+    return s1;
+    
+}
