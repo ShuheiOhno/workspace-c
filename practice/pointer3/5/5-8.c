9e1c21b60d5490d9fe46f59c8ef8d678badd0f3d
ShuheiOhno <inu.suika8@gmail.com>
2024-02-04 12:45:43 +0900

    d

--- practice/pointer3/5/5-8.c
+++ practice/pointer3/5/5-8.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    for (int i = 0; i < argc; i++)
+    int i;
+    for (i = 0; i < argc; i++)
         printf("argv[%d]=%s\n", i, argv[i]);
+    printf("argc:%d\n", argc);
+    printf("---------------\n");
+    i = 0;
+    while(argc-- > 0)
+        printf("argv[%d]=%s\n", i++, *argv++);
     printf("argc:%d\n", argc);
 }
