6939941c9d6675b7901bf1ccde7239309e18fcf5
ShuheiOhno <inu.suika8@gmail.com>
2024-02-05 21:52:16 +0900

    Update 6-4.c

--- practice/pointer3/6/6-4.c
+++ practice/pointer3/6/6-4.c
@@ -9,12 +9,12 @@ typedef struct {
 int main(void) {
     XYZ a;
     printf("a.x:"); scanf("%d", &a.x);
-    printf("a.y:"); scanf("%d", &a.y);
-    printf("a.z:"); scanf("%d", &a.z);
+    printf("a.y:"); scanf("%ld", &a.y);
+    printf("a.z:"); scanf("%lf", &a.z);
 
     printf("a.x=%d\n", a.x);
-    printf("a.y=%d\n", a.y);
-    printf("a.z=%d\n", a.z);
+    printf("a.y=%ld\n", a.y);
+    printf("a.z=%.2f\n", a.z);
 
     return 0;
 }
