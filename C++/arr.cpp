8037218fd05c76df583d1b8da46c2fbca54e9d74
ShuheiOhno <inu.suika8@gmail.com>
2024-02-25 12:51:45 +0900

    s

--- C++/arr.cpp
+++ C++/arr.cpp
@@ -3,15 +3,16 @@
 
 int main() {
     int num;
-    int arr[num];
-
     std::cout << "要素数は：";
     std::cin >> num;
+
+    int arr[num];
+    
     for(int i = 0; i < num; i++) {
         std::cin >> arr[i];
     }
     for (int i = 0; i < num; i++) {
-        std::cout << arr[i];
+        std::cout << i << "番目:" << arr[i] <<"\n";
     }
 
     return 0;
